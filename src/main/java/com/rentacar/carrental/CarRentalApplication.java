2c5268b5dd41008dff1a72bb861c5d110ca5cf8f
Hasan Deniz Dogan <hasandenizdogan@gmail.com>
2021-11-22 20:51:36 +0300

    added environment variables for maven tests

--- src/main/java/com/rentacar/carrental/CarRentalApplication.java
+++ src/main/java/com/rentacar/carrental/CarRentalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CarrentalApplication {
+public class CarRentalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CarrentalApplication.class, args);
+		SpringApplication.run(CarRentalApplication.class, args);
 	}
 
 }
