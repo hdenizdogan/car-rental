db3116a6f6e9d9219c4c5ac311cd76308a7743e2
Hasan Deniz Dogan <hasandenizdogan@gmail.com>
2021-11-23 03:46:26 +0300

    date input format has been updated to dd/MM/yyyy

--- src/main/java/com/rentacar/carrental/model/Person.java
+++ src/main/java/com/rentacar/carrental/model/Person.java
@@ -1,16 +1,12 @@
 package com.rentacar.carrental.model;
 
-import lombok.AccessLevel;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.MappedSuperclass;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.Date;
 
 @Setter
 @Getter
@@ -19,9 +15,11 @@ public class Person extends BaseEntity{
 
     @Column(name = "first_name")
     private String firstName;
+
     @Column(name = "last_name")
     private String lastName;
 
+    @JsonFormat(pattern = "dd/MM/yyyy")
     @Column(name="date_of_birth")
     private LocalDate dateOfBirth;
 }
